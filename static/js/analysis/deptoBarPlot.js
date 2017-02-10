7226f544a194492daac20ac2af19ae54b5487309
fnbellomo <fnbellomo@gmail.com>
2017-02-10 03:11:06 -0300

    the barplot is resposibe

--- static/js/analysis/deptoBarPlot.js
+++ static/js/analysis/deptoBarPlot.js
@@ -1,12 +1,22 @@
+var myChart;
+
 $(document).ready(function() {
-    var svg = dimple.newSvg("#deptoPlot", 590, 400);
+    var svg = d3.select("#deptoPlot");
+
     d3.csv("analisis/datos/cantidad_por_departamento.csv",
 	   function (data) {
-	       var myChart = new dimple.chart(svg, data);
-	       myChart.setBounds(60, 30, 510, 305)
+	       myChart = new dimple.chart(svg, data);
+
+	       // Fix the margins
+	       myChart.setMargins("60px", "30px", "110px", "70px");
+
 	       var x = myChart.addCategoryAxis("x", "Departamento");
 	       myChart.addMeasureAxis("y", "Cortaderos por departamento");
 	       myChart.addSeries(null, dimple.plot.bar);
 	       myChart.draw();
 	   });
 })
+
+window.addEventListener('resize', function() {
+    myChart.draw();
+});
